9d74b5bfd65e8ae00ad1e74f171b19e5f8827758
jplavorr <jplavorr@gmail.com>
2023-06-29 20:51:33 -0300

    mandando tudo inicialmente

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, jsonify, request
+from sqlalchemy import create_engine
+import pandas as pd
+import json
+import psycopg2
+from sqlalchemy import text
+
+app = Flask(__name__)
+
+# Criação da conexão
+#engine = create_engine('postgresql://postgres:jplavorr1103@localhost:5432/postgres')
+engine = create_engine('postgresql://postgres:jpseedz123@104.154.44.235:5432/weather')
+
+
+
+@app.route('/')
+def home():
+    return "Seedz"
+
+@app.route('/api/weather', methods=['GET'])
+def get_weather_data():
+    # Leitura do schema
+    query = "SELECT * FROM weather.grid_weather_data"  
+    df = pd.read_sql_query(query, con=engine)
+
+    # Converte o DataFrame em um dicionário e, em seguida, em JSON
+    return jsonify(df.to_dict(orient='records'))
+
+@app.route('/api/jsondata', methods=['GET'])
+def get_json_data():
+    with open('grid_weather.json') as f:
+        data = json.load(f)
+
+    # Retorna os dados JSON
+    return jsonify(data)
+
+
+@app.route('/api/create_weather', methods=['POST'])
+def create_weather_data():
+    data = request.get_json()  # Obter dados JSON do corpo da requisição
+
+    df = pd.DataFrame([data])  # Cria um DataFrame com os dados
+    df.to_sql('weather.grid_weather_data', con=engine, if_exists='append', index=False)  # Inserir os dados na tabela
+
+    return jsonify({'message': 'Dados inseridos com sucesso!'}), 201
+
+@app.route('/api/update_weather/<int:cod_city>', methods=['PUT'])
+def update_weather_data(cod_city):
+    data = request.get_json()  # Obter dados JSON do corpo da requisição
+
+    df = pd.DataFrame([data])  # Cria um DataFrame com os dados
+    df.to_sql('weather.grid_weather_data', con=engine, if_exists='replace', index=False)  # Atualizar os dados na tabela
+
+    return jsonify({'message': 'Dados atualizados com sucesso!'})
+
+@app.route('/api/delete_weather/<int:cod_city>', methods=['DELETE'])
+def delete_weather_data(cod_city):
+    with engine.connect() as connection:
+        connection.execute(text(f"DELETE FROM weather.grid_weather_data WHERE cod_city = {cod_city};"))  # Exclui os dados na tabela
+
+    return jsonify({'message': 'Dados excluídos com sucesso!'})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
---
+++ request.py
@@ -0,0 +1,48 @@
+import requests
+import concurrent.futures
+
+data_to_post = {
+    "cod_city": 123456,
+    "date": "2023-06-29",
+    "hour": 12,
+    "precipitation": 1.0,
+    "dry_bulb_temperature": 25.0,
+    # adicione o restante dos campos aqui
+}
+
+data_to_put = {
+    "cod_city": 123456,
+    "date": "2023-06-29",
+    "hour": 13,
+    "precipitation": 2.0,
+    "dry_bulb_temperature": 26.0,
+    # adicione o restante dos campos aqui
+}
+
+cod_city_to_delete = 123456
+
+def create_weather_data(url, data):
+    response = requests.post(url, json=data)
+    print(f'Response from POST {url}: status code - {response.status_code}, response - {response.json()}')
+
+def get_weather_data(url):
+    response = requests.get(url)
+    print(f'Response from GET {url}: status code - {response.status_code}, response - {response.json()}')
+
+def update_weather_data(url, data):
+    response = requests.put(url, json=data)
+    print(f'Response from PUT {url}: status code - {response.status_code}, response - {response.json()}')
+
+def delete_weather_data(url):
+    response = requests.delete(url)
+    print(f'Response from DELETE {url}: status code - {response.status_code}, response - {response.json()}')
+
+
+# Crie um executor de thread pool
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    # Inicie várias threads que irão executar as operações CRUD
+    executor.submit(create_weather_data, 'http://localhost:5000/api/create_weather', data_to_post)
+    #executor.submit(get_weather_data, 'http://localhost:5000/api/weather')
+    executor.submit(update_weather_data, f'http://localhost:5000/api/update_weather/{data_to_put["cod_city"]}', data_to_put)
+    executor.submit(delete_weather_data, f'http://localhost:5000/api/delete_weather/{cod_city_to_delete}')
+
